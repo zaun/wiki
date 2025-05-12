15a661e4900c729c759c0e6274df31c02d8da0d4
Justin Zaun <justin.zaun@gmail.com>
2025-05-12 10:58:43 -1000

    update run for new env add a .gitignore

--- wiki-backend/run.sh
+++ wiki-backend/run.sh
@@ -13,4 +13,3 @@ podman run -d --name wiki-backend \
   -e RECOVERY_KEY_SALT=9db9b172072a615f04b8998425c0025a95736c71f59245a88fbbeb6f12d93e51 \
   -e JWT_SECRET=a7e976d3628c625b03601295a503fc4368126529157c2737579ef8844031998c \
   registry.jgz.guru/wiki-backend
-  
